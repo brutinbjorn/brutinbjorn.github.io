868ed5425c6566d5b072ac041287a0fc257c45df
brutinbjorn <brutinbjorn@gmail.com>
2024-09-02 21:05:42 +0200

    fixed issues with broken files.

---
+++ assets/GameProjects-77449d70.js
@@ -0,0 +1,143 @@
+import{P as e,a as s}from"./ProjectData-f5468549.js";import{f as o,_ as n,r as i,c,d as l,a,b as d,o as p}from"./index-390ce750.js";const h=[new e("project-1","Replaceable","img/projects/Replaceable_Icon.jpg",`
+    <div class="paragraph">
+     <strong>Replaceable</strong> The video Game being created at The Pack Studios. <br/>
+     I Worked on this project as a Game Developer, where i worked on new game mechanics, optimization & refactoring of the code. </br>
+     I started as a intern in September until halfway January, and then was hired to continue on this work until June the 30th. </br>
+     <!--// is a thing of beauty that I am so proud of. It's available on Android because why not.
+     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>. -->
+    </div>
+    <!--<div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+    </div>-->
+    <div class="paragraph center">
+        <iframe class="video-container" src="vid/movie_max_vp9.webm" height="300px" width="480px"></iframe>
+    </div>
+
+    <!-- example of linking to the app store
+    <div class="paragraph center">
+        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    </div>-->
+    
+    <div class="paragraph">
+        Main features :
+        <ul>
+            <li>Control different robots, each with there own mechanic's.</li>
+            <li>Hack objects in the enviroment.</li>
+            <li>Evade, trick or disable hostile robots.</li>
+            <!--<li>And then some</li>-->
+        </ul>
+    </div>
+
+    <div class="notice">
+        the game can be found on <a href="https://store.steampowered.com/app/2145980/Replaceable/" target="_blank"> steam.</a>
+    </div>
+    
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="/img/projects/Replaceable_Gameplay1.jpg" alt="Replaceable gameplay 1 screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Replaceable_Gameplay2.jpg" alt="Replaceable gameplay 2 screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Replaceable_Gameplay3.jpg" alt="Replaceable gameplay 3 screenshot" />
+    </div>
+
+    `,"#0033cc",!0),new e("project-2","Slumberland","img/projects/SlumberLand_Icon.png",`
+    <div class="paragraph">
+        <strong>Slumberland</strong> Was made in my 3rd year of DAE, where i worked in a Team off 7. </br>
+        Me and a fellow programmer, 3 Artist, 2 Independant Game Producers, and a animator. </br>
+        Made for MyMachine, a global orginization that helps children in there education. </br>
+
+        <!--<br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.-->
+    </div>
+    <div class="paragraph">
+        The idea and general method that MyMachine uses is to have children between 6-12 think of a dream machine, and have older ages actually create it. </br>
+        For this project then, we interviewed a 2 classes in the 6th grade, and had them create there "Dream machines", we then started on this project with 50 different drawings we had to convert in to game. </br>
+        When then decided as a team to create a adventure game where we work all of the drawings into the enviroment, game mechanics and enemy's. </br>
+        This is the final result of 5 months working on the game.
+    </div>
+
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/l3_uSAb8DKI" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Explore a magical forest, and a toy factory.</li>
+        <li>Fight nightmares invading the Dreamland.</li>
+        <li>Collect the 52 drawings of the children, created for there dream machine's.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+    <div class="notice">
+        Windows build available on <a href="https://reinard-baertsoen.itch.io/mymachine" target="_blank">itch.io</a>.
+        <!--Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.-->
+    </div>
+    </div>
+
+    <div class="paragraph center">
+    <img class="pc-screenshot" src="/img/projects/Slumberland_Icon_Game1.png" alt="In game screenshot 1" />
+    <img class="pc-screenshot" src="/img/projects/Slumberland_Icon_Game2.png" alt="In game Screenshot 2" />
+    <img class="pc-screenshot" src="/img/projects/Slumberland_Icon_Game3.png" alt="In game Screenshot 3" />
+    <img class="pc-screenshot" src="/img/projects/Slumberland_Icon_Game4.png" alt="In game Screenshot 4" />
+    </div>
+    `,"#5a78af"),new e("project-3","Dont let them eat cake.","img/projects/DontLetThemEatCake_Icon.png",`
+    <div class="paragraph">
+        <strong>Dont let them eat cake.</strong>Was made during the 2022 DAE Game in witch Me, and 4 other students game together to create a game in 1 week. </br>
+        Me as the programmer, 1 student from Independant game production and 3 Artist. </br>
+        As one of our team mates had access and started messing around with VR, We decided to create a VR-Game. <br/>
+        with this game being the result, and I'm pretty proud of it. </br>
+
+        <!--<br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.-->
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/xPWpQKiAGSU" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>VR Experience</li>
+        <li>Protect your Cake by placing cupcakes.</li>
+        <li>Balance your defences, or break the table.</li>
+        <li>Defeat the waves of cute hungry invaders.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        You can download the game on <a href="https://hary766.itch.io/dont-let-them-eat-cake" target="_blank">itch.io</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay1.jpg" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay2.png" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay3.png" alt="Drawing Overload Screenshot" />
+        <!--<img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" /> -->
+    </div>
+    `,"#00ff99"),new e("project-4","Underfed","img/projects/Underfed_Icon.png",`
+    <div class="paragraph">
+    <strong>Underfed</strong> Made in my second year of DAE as part of the Game project course.</br>
+    Together with 5 other student from DAE. 
+    </div>
+    
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Serve food to customer.</li>
+        <li>Keep the restaurant tidy.</li>
+        <li>Try to get the best score possible.(or not)</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay1.jpg" alt="Underfed gameplay screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay2.png" alt="Underfed gameplay screenshot" />
+        <img class="pc-screenshot" src="/img/projects/Underfed_Gameplay3.png" alt="Underfed gameplay screenshot" />
+    </div>
+    `,"#e80fb7")],m=o({name:"GameProjects",components:{ProjectsList:s},data:function(){return{projects:h}}}),g=a("h1",null,"Games I worked on.",-1),f=a("div",{style:{"margin-bottom":"30px"}}," The following are some Games I've made or heavily contributed to. ",-1),b=a("div",{style:{"margin-top":"20px"}},[d(" There is more to see on "),a("a",{target:"_blank",href:"https://someexternalwebsite.com"},"some external website")],-1);function u(t,v,w,y,_,j){const r=i("ProjectsList");return p(),c("div",null,[g,f,l(r,{projects:t.projects},null,8,["projects"]),b])}const I=n(m,[["render",u]]);export{I as default};
---
+++ assets/OtherProjects-0bd92f79.js
@@ -0,0 +1,95 @@
+import{P as e,a as s}from"./ProjectData-f5468549.js";import{f as i,_ as o,r as n,c,d as h,a as t,o as l}from"./index-390ce750.js";const d=[new e("project-6","MiniArcade C++","img/projects/Cursed_Arcade.jpeg",`
+    <div class="paragraph">
+      <strong>MiniArcade</strong> was made in my 4th semester of DAE. <br/>
+      written in C++, MiniArcade is a solution that contain 2 projects in it. <br/>
+      1 containing a small video game engine, and A video game using that engine. <br/>
+      the Engine is called Minilord, and functions as a library that handles updating, the game objects, the scenes etc. <br/>
+      the  game project called The Cursed arcade, uses the engine, and adds the the logic for the video game on top. <br/>
+      using these 2 projects I created this little game. <br/>
+      all of this code has been written by me. <br/>
+      <br/>
+      Image by <a href="https://www.pexels.com/@technobulka/" target="blank">Stanislav Kondratiev</a>.
+
+          <div class="paragraph center">
+            <iframe class="video-container" src="vid/videoWithoutAudio.mkv" height="300px" width="480px"></iframe>
+          </div>
+
+          <div class="paragraph">
+            Main features :
+            <ul>
+            <li>Keeps track of the entire keyboard, and controller support.</li>
+            <li>Nested gameobjects, each with there own collection of componenents.</li>
+            <li>Contains SDL2, json, glm, ImGUI, and can use steamworks.</li>
+            </ul>
+        </div>
+        <div class="notice">
+          the project can be found on <a href="https://github.com/brutinbjorn/MiniArcade" target="_blank"> GitHub.</a>
+      </div>
+
+        <!--<div class="paragraph center">
+          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
+          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
+          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
+        </div>-->
+    `,"#c10606",!1,!0),new e("project-7","Rasterizer And Raytracer C++","img/projects/Screenshot_Rasterizer_Small.png",`
+    <div class="paragraph">
+                <strong>Rasterizer And Raytracer</strong> Was created during my 3 Semester of DAE, in the 1st graphics programming module.
+                this is the result of that course. I learned the basic technices and math behind rendering a 3D module.
+                First learning how a basic raytracer works, and then a basic Rasterizer.
+            </div>
+
+            <div class="paragraph">
+              Main features :
+              <ul>
+              <li>Raytracer and Rasterizer made from scratch, written in C++.</li>
+              <li>Lights & Ability to use different texture techniques: color, Normal, Occlusion .</li>
+              </ul>
+          </div>
+
+            <div class="paragraph">
+              <div class="notice">
+              The project can be found on <a href="https://github.com/brutinbjorn/MiniArcade" target="_blank"> GitHub.</a>          
+              </div>
+            </div>
+
+            <!--<div class="paragraph center">
+                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
+                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
+                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" /> 
+            </div>-->
+            `,"#1ca1e2"),new e("project-8","Zombie survival AI C++","img/projects/Screenshot_ZombieAI.png",`
+            <div class="paragraph">
+                A <strong>Zombie survival AI</strong> Made during my game play programming course.<br/>
+                This exam project interfaces with already build project, that generates the enviroment.<br/>
+                My part was then to give the player character a AI so that it could survive as long as possible.<br/>
+                this is the result. <br/>
+            </div>
+
+            <div class="paragraph center">
+              <iframe class="video-container" src="vid/CompressedZombieAI.mp4"></iframe>
+            </div>
+            <div class="paragraph">
+              the AI will look for houses first.<br/> 
+              Thisis done by abusing the fact that the player character checks the navmesh for points it cant go straight towards.<br/>
+              Then the AI navigates towards the closest house, checking the items it finds inside and grabs a specific layout.<br/>
+              After grabbing a pistol, it will also fire at enemy's. when hit the character will also check if there are any enemy's behind him. <br/>
+              The AI uses a Behaviour tree to decide its action's.<br/>
+            </div>
+            <div class="paragraph">
+              <div class="notice">
+              The project can be found on <a href="https://github.com/brutinbjorn/ZombieSurvivalAI" target="_blank"> GitHub.</a>
+              </div>
+            </div>
+            <!--
+            <div class="paragraph center">
+              <a href="https://play.google.com/store/apps/details?id=someapp" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+            </div>
+            -->
+
+            <!--
+            <div class="paragraph center">
+                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
+                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
+            </div>
+            -->
+            `,"#1ca1e2",!1,!0)],p=i({name:"OtherProjects",components:{ProjectsList:s},data:function(){return{projects:d}}}),g=t("h1",null,"Other stuff",-1),m=t("div",{style:{"margin-bottom":"30px"}}," And here are some other stuff I've made or heavily contributed to, which are totally different than the stuff from earlier. ",-1);function u(a,f,b,v,j,w){const r=n("ProjectsList");return l(),c("div",null,[g,m,h(r,{projects:a.projects},null,8,["projects"])])}const A=o(p,[["render",u]]);export{A as default};
